7476085dd04614dbf6a50ba16f1c2812f4fe14fd
Diego Alfredo Molina <molinadiego95@outlook.com>
2024-09-13 15:53:01 -0600

    Added comments to explain the statements

--- UsingSelect/select_all_columns_from_table.sql
+++ UsingSelect/select_all_columns_from_table.sql
@@ -1,4 +1,10 @@
 USE SlickShop;
+--Selecting all the Columns from a Table.
 
+--Manually Asking for each individual column.
+SELECT PartID, Description, Cost
+    FROM Part;
+
+-- Using the * Expression to Ask for all of them
 SELECT *
     FROM Part;
