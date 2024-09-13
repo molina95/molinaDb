a1014d65c6eb6fb87d2d4116303dca0233731620
Diego Alfredo Molina <molinadiego95@outlook.com>
2024-09-13 15:14:09 -0600

    Added one select that uses two tables.

--- UsingSelect/MyPersonalTests.sql
+++ UsingSelect/MyPersonalTests.sql
@@ -13,3 +13,8 @@ FROM StateOrProvince;
 
 SELECT StateOrProvince
 FROM Customer;
+
+
+--Takes one customer row and joins it with each of the rows in stateorprovince regardless of what the customer stateorprovince is.
+SELECT CustomerID, Customer.StateOrProvince, StateOrProvince.StateOrProvince
+FROM Customer, StateOrProvince;
