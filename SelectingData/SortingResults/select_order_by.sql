1f8e2d4112ad979224faa0c8c1941e31922cc0c4
Diego Alfredo Molina <molinadiego95@outlook.com>
2024-09-13 17:01:58 -0600

    Added examples for using the ORDER BY clause in ASCENDING or DESCENDING order.

--- SelectingData/SortingResults/select_order_by.sql
+++ SelectingData/SortingResults/select_order_by.sql
@@ -11,5 +11,25 @@ ORDER BY 2;
 ORDER BY PartID;
 
 --Both give the same result but you can see how the
---ORDER BY Clause needs a reference in the result set or the Table to order the rows.
+--ORDER BY Clause needs a reference in the result set or the FROM Tables to order the rows.
 
+    SELECT  Description
+        FROM Part
+ORDER BY  Cost;
+
+--Here you can  see the use of the ASC and the DES keyword
+--By default SQL Orders in ascending order unless you give it
+--The DES keyword for descending order.
+
+    SELECT PartID, Description, Cost
+        FROM Part
+ORDER BY  Cost ASC;
+
+    SELECT PartID, Description, Cost
+        FROM Part
+ORDER BY  Cost DESC ;
+
+--This result set should be the same as the one in Ascending order.
+    SELECT PartID, Description, Cost
+        FROM Part
+ORDER BY  Cost;
