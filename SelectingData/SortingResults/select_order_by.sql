71844d7c731f2e6b172172278806987b63704750
Diego Alfredo Molina <molinadiego95@outlook.com>
2024-09-13 17:10:53 -0600

    Added more examples of how to use the colum position to order the rows of the result set. In this case when using SELECT with the * expression.

--- SelectingData/SortingResults/select_order_by.sql
+++ SelectingData/SortingResults/select_order_by.sql
@@ -10,12 +10,34 @@ ORDER BY 2;
         FROM Part
 ORDER BY PartID;
 
---Both give the same result but you can see how the
+--Both give the same result, but you can see how the
 --ORDER BY Clause needs a reference in the result set or the FROM Tables to order the rows.
 
     SELECT  Description
         FROM Part
 ORDER BY  Cost;
+
+-- For example let's use the * expression to get all the columns from a table
+-- and the column position to order it by.
+
+--Keep in mind that the Natural order of the table is
+--PartId, Description and Cost
+
+--Will order them by PartId
+    SELECT *
+        FROM Part
+ORDER BY 1;
+
+--Will order them by Description
+    SELECT *
+        FROM Part
+ORDER BY 2;
+
+--Will order them by Cost
+    SELECT *
+        FROM Part
+ORDER BY 3;
+
 
 --Here you can  see the use of the ASC and the DES keyword
 --By default SQL Orders in ascending order unless you give it
