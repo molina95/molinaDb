ec799997d51ecf1bcc72d65fc58ba24f0462a409
Diego Alfredo Molina <molinadiego95@outlook.com>
2024-09-13 15:47:27 -0600

    Created select_distinct_expression.sql to hold simple examples of how the DISTINCT expression works in a Select statement.

---
+++ UsingSelect/select_distinct_expression.sql
@@ -0,0 +1,14 @@
+USE SlickShop;
+
+-- Selects all the Costs from the Part Table
+SELECT Cost
+    FROM Part;
+
+--Selects only the unique Costs from the Part Table
+SELECT DISTINCT Cost
+    FROM Part;
+
+--Selects only the Distinct StateOrProvinces from the Customer Table.
+SELECT DISTINCT StateOrProvince
+    FROM Customer;
+
