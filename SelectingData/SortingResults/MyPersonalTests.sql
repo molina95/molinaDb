71af3fa9a1c7c9a71125cc05c66706a32a19c8ba
Diego Alfredo Molina <molinadiego95@outlook.com>
2024-09-13 17:49:08 -0600

    Added an example of aliases in MyPersonalTests.sql

--- SelectingData/SortingResults/MyPersonalTests.sql
+++ SelectingData/SortingResults/MyPersonalTests.sql
@@ -3,12 +3,12 @@ Use SlickShop
 --You can do combinations of Columns when using the DISTINCT clause.
 --Here we can get all the unique combinations of StateOrProvince and PostalCode.
 SELECT DISTINCT StateOrProvince, PostalCode
-    FROM Customer;
+FROM Customer;
 
 --You can take Distinct result sets and order them.
 --But you need a reference in the result set of the row you want to order them by.
-    SELECT DISTINCT Cost
-        FROM Part
+SELECT DISTINCT Cost
+FROM Part
 ORDER BY Cost DESC;
 
 /* This Won't work when using the DISTINCT clause
@@ -16,3 +16,8 @@ ORDER BY Cost DESC;
         FROM Part
 ORDER BY  Cost ASC;
 */
+
+--Here I tried to use Aliases on this Select statement that had two similar column names.
+SELECT CustomerID, Customer.StateOrProvince AS "Customer's State", StateOrProvince.StateOrProvince AS State
+FROM Customer,
+     StateOrProvince;
